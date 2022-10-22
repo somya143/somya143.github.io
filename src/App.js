40ff56293e1491b30af66856a2376bd94a3f0def
somya ranjan singh <maaheersj@gmail.com>
2022-10-22 17:52:27 +0530

    potfolio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./Components/Navbar";
 import About from "./Components/About";
+import Skills from "./Components/Skills";
 
 function App() {
   return (
     <div className="App">
         <Navbar />
         <About />
+        <Skills />
     </div>
   );
 }
