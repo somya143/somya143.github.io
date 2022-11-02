cdd0c5b85e27ce5794eac1c0d9b29109be981a95
somya ranjan singh <maaheersj@gmail.com>
2022-11-02 14:02:16 +0530

    portfolio

--- src/Github/Stats.jsx
+++ src/Github/Stats.jsx
@@ -10,25 +10,25 @@ export const Stats = () => {
             <div>
             <div className="statsContainer">
                 <div>
-                    <a href="https://github.com/lokesh-patidar">
-                        <img align="left" src="https://github-readme-streak-stats.herokuapp.com/?user=lokesh-patidar" />
+                    <a href="https://github.com/somya143">
+                        <img align="left" src="https://github-readme-streak-stats.herokuapp.com/?user=somya143" />
                     </a>
                 </div>
             </div>
 
             <div className="stats-3">
                 <p align="center">
-                <a href="https://github.com/lokesh-paidar">
-                        <img align="center" src="https://github-readme-stats.vercel.app/api/top-langs/?username=lokesh-patidar&bg_color=0D1117" alt="" />
+                <a href="https://github.com/somya143">
+                        <img align="center" src="https://github-readme-stats.vercel.app/api/top-langs/?username=somya143&bg_color=0D1117" alt="" />
                     </a>
                 </p>
             </div>
 
             <div className="stats-2">
                 <p align="center">
-                    <a href="https://github.com/lokesh-patidar/github-readme-stats">
-                        <img alt="lokesh-patidar Github Stats" 
-                        src="https://github-readme-stats.vercel.app/api?username=lokesh-patidar&show_icons=true&locale=en&theme=react&hide_border=true&bg_color=0D1117" 
+                    <a href="https://github.com/somya143/github-readme-stats">
+                        <img alt="Somya Ranjan Singh Github Stats" 
+                        src="https://github-readme-stats.vercel.app/api?username=somya143&show_icons=true&locale=en&theme=react&hide_border=true&bg_color=0D1117" 
                         />
                     </a>
                 </p>
