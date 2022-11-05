d90f62e460761737845ccf7ba2424617635cf1f0
somya ranjan singh <maaheersj@gmail.com>
2022-11-05 21:04:03 +0530

    portfolio

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -101,6 +101,23 @@ export default function Navbar() {
             <Button onClick={toggleColorMode}>
               {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
             </Button>
+
+            <Button
+              rounded={"full"}
+              bg={"blue.400"}
+              color={"white"}
+              _hover={{
+                bg: "blue.500",
+              }}
+            >
+              <Link
+                style={{ textDecoration: "none" }}
+                href="https://drive.google.com/file/d/1kXEj9rY4ceJuA4temChJ-qZVIxgcPlzu/view?usp=sharing"
+                target={"_blank"}
+              >
+                My Resume
+              </Link>
+            </Button>
             <Button
               rounded={"full"}
               bg={"blue.400"}
