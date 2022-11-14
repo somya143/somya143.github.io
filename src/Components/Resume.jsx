bb810a6e97112a1f844155c0bb0b1ac090981749
somya ranjan singh <maaheersj@gmail.com>
2022-11-14 12:23:37 +0530

    portfolio

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import {useNavigate} from 'react-router-dom'
 
 const Resume = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("https://drive.google.com/file/d/1kXEj9rY4ceJuA4temChJ-qZVIxgcPlzu/view?usp=share_link")
+  }
+
   return (
-    <div>Resume</div>
+    <div onClick={handleClick}>
+    Resume
+    </div>
   )
 }
 
