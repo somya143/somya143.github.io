d5c0e999492dbf3f55cc317fdfa302d99537a521
somya ranjan singh <maaheersj@gmail.com>
2023-04-24 18:57:10 +0530

    updated resume

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Resume = () => {
 
   const downloadResume = () => {
-    fetch('Somya Ranjan Singh.pdf').then(response => {
+    fetch('Somya_Ranjan_Singh_Resume.pdf').then(response => {
       response.blob().then(blob => {
           // Creating new object of PDF file
           const fileURL = window.URL.createObjectURL(blob);
           // Setting various property values
           let alink = document.createElement('a');
           alink.href = fileURL;
-          alink.download = 'Somya Ranjan Singh.pdf';
+          alink.download = 'Somya_Ranjan_Singh_Resume.pdf';
           alink.click();
       })
   })
